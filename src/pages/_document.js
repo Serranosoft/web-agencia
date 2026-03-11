e1ccc7184f119b9cfd604f3b67921aa98b33a1ae
Paula Quintana <paulaquintanagon00@gmail.com>
2026-03-11 11:57:25 +0100

    Feat: Aañdir formulario de typeform con botón flotante integrado en la web

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,8 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <div data-tf-live="01KKE8AVY3WRE05Y7AN1QF1G1E"></div>
+        <Script src="//embed.typeform.com/next/embed.js" strategy="lazyOnload" />
       </body>
     </Html>
   )
